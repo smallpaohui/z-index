268a019e077f4966a320374769ed5497b881c08e
张_啸_天 <zxt9765@163.com>
2021-01-29 09:43:14 +0800

    NProgress

--- plugin/axios.js
+++ plugin/axios.js
@@ -9,7 +9,7 @@ NProgress.configure({
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
-  NProgress.start()
+  NProgress.start()//开启进度条
   return config
 }, function (error) {
   // 对请求错误做些什么
@@ -19,7 +19,7 @@ axios.interceptors.request.use(function (config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
   // 对响应数据做点什么
-  NProgress.done()
+  NProgress.done()//关闭进度条
   return response
 }, function (error) {
   // 对响应错误做点什么
